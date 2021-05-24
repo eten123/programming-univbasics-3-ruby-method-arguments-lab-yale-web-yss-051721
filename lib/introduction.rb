71ca1146a65b3cf1fe94aacbbb6aa966589d24eb
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-24 21:23:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,9 +9,8 @@ def introduction(name = "Jim")
 end
 introduction(name)
 
-def introduction_with_language (name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
-end
+#def introduction_with_language (name,language)
+  ##end
 introduction_with_language("Kim", "Ruby")
 
 
